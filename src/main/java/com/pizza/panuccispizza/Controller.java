f4fd2d04a926c03eb45b36daf55c35ac9064e52e
Hunter Dorminey <hdormine@students.kennesaw.edu>
2021-12-03 23:14:27 -0500

    Gave buttons labels.

--- src/main/java/com/pizza/panuccispizza/Controller.java
+++ src/main/java/com/pizza/panuccispizza/Controller.java
@@ -1,19 +1,29 @@
 package com.pizza.panuccispizza;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Controller {
 
+//Initial Setup
     private Stage stage;
     private Scene scene;
     private Parent root;
+
+//Build-a-pizza
+    @FXML
+    private ChoiceBox toppingOne;
+    private ChoiceBox toppingTwo;
+    private ChoiceBox toppingThree;
+    private ChoiceBox toppingFour;
 
     public void switchToPrimary(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("builderScene.fxml"));
