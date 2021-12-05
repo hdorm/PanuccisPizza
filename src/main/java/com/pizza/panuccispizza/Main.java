3c0cfe593ce139b60f9506e6c212f4327e456515
Hunter Dorminey <hdormine@students.kennesaw.edu>
2021-12-04 20:40:56 -0500

    Added title and pizza icon to program.

--- src/main/java/com/pizza/panuccispizza/Main.java
+++ src/main/java/com/pizza/panuccispizza/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -13,6 +14,8 @@ public class Main extends Application {
         try {
             Parent root = FXMLLoader.load(getClass().getResource("loginScene.fxml"));
             Scene scene = new Scene(root);
+            stage.setTitle("Panucci's Pizza");
+            stage.getIcons().add(new Image("/tinypizzaicon.png"));
             stage.setScene(scene);
             stage.show();
         } catch(Exception e) {
