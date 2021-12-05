4c665bee6eab8a1fc08869b0e9a4227eb58a137e
Hunter Dorminey <hdormine@students.kennesaw.edu>
2021-12-04 19:03:39 -0500

    Added options to drop down menus.

--- src/main/java/com/pizza/panuccispizza/switchController.java
+++ src/main/java/com/pizza/panuccispizza/switchController.java
@@ -1,39 +1,29 @@
 package com.pizza.panuccispizza;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ChoiceBox;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
-public class Controller {
+public class switchController {
 
-//Initial Setup
+    //Initial Setup
     private Stage stage;
     private Scene scene;
-    private Parent root;
 
-//Build-a-pizza
-    @FXML
-    private ChoiceBox toppingOne;
-    private ChoiceBox toppingTwo;
-    private ChoiceBox toppingThree;
-    private ChoiceBox toppingFour;
-
+    //Moves from one Scene to the other
     public void switchToPrimary(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("builderScene.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("primaryScene.fxml"));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
-    public void switchToFinal(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("pizzaFinal.fxml"));
+    public void switchToReceipt(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("receiptScene.fxml"));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
